2b68e7df1a52136f886c156d8ae58322437e3e73
chrstnale <christianale85@gmail.com>
2021-01-28 17:05:12 +0700

    Menambahkan Tugas 12 Component

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import Component from './Tugas/Tugas12/App';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Component />
+    // <View style={styles.container}>
+    //   <Text>VISA RDVS</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
